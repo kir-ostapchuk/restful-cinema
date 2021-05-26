91a6a4df21d64ed8a7899d5d4b333df2a936d8bc
Kirill Ostapchuk <k.ostapchuk@itransition.com>
2021-05-26 19:02:11 +0300

    refactor: redo tests for controller

--- src/test/kotlin/com/itransition/web/cinema/UserControllerTests.kt
+++ src/test/kotlin/com/itransition/web/cinema/UserControllerTests.kt
@@ -1,134 +1,147 @@
 package com.itransition.web.cinema
 
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
+import com.itransition.web.cinema.model.User
+import com.itransition.web.cinema.service.UserService
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
+import org.mockito.Mockito
+import org.mockito.kotlin.any
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup
-import org.springframework.web.context.WebApplicationContext
 
 @SpringBootTest
-@TestMethodOrder(
-    MethodOrderer.OrderAnnotation::class)
+@AutoConfigureMockMvc
 class UserControllerTests {
 
-    // TODO(
-    //  Replace crazy code:
-    //  4. add comments
-    //  5. remove string-json
-    //  6. add comparing json with
-    //  7. test all cases (non-valid name, resource does not exist)
-    //  )
-
-    private val baseUrl = "http://localhost:8080/api/v1/users"
-    private val jsonContentType = MediaType(MediaType.APPLICATION_JSON.type, MediaType.APPLICATION_JSON.subtype)
+    @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @Autowired
-    private lateinit var webAppContext: WebApplicationContext
+    @MockBean
+    private lateinit var userService: UserService
 
-    @BeforeEach
-    fun before() {
-        mockMvc = webAppContextSetup(webAppContext).build();
+    @Test
+    fun `Should add user`() {
+
+        // Given
+        val user = User(1, "qjnzero", "lolpassword", "Kirill", 21)
+        val jsonBody = """
+            {
+                "login": "qjnzero",
+                "password": "lolpassword",
+                "firstName": "Kirill",
+                "age": 21
+            }
+        """.trimIndent()
+
+        // When
+        Mockito.`when`(userService.save(any())).thenReturn(user)
+
+        // Verify
+        mockMvc.perform(
+            post("/api/v1/users")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.login").value("qjnzero"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.password").value("lolpassword"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Kirill"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(21))
     }
 
     @Test
-    @Order(1)
-    fun `1 - Get empty list of users`() {
-        val request = get(baseUrl).contentType(jsonContentType)
+    fun `Should not add user when add user with empty login and password`() {
 
-        mockMvc.perform(request)
-            .andExpect(status().is2xxSuccessful)
+        // Given
+        val jsonBody = """
+            {
+                "login": "",
+                "password": "",
+                "firstName": "Vasya",
+                "age": 14
+            }
+        """.trimIndent()
+
+        // Verify
+        mockMvc.perform(
+            post("/api/v1/users")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `Should return user`() {
+
+        // Given
+        val user = User(1, "qjnzero", "lolpassword", "Kirill", 21)
+
+        // When
+        Mockito.`when`(userService.findOne(1)).thenReturn(user)
+
+        // Verify
+        mockMvc.perform(get("/api/v1/users/1"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType("application/json"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.login").value("qjnzero"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.password").value("lolpassword"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Kirill"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(21))
+    }
+
+    @Test
+    fun `Should return empty list`() {
+
+        // Verify
+        mockMvc.perform(get("/api/v1/users"))
+            .andExpect(status().isOk)
             .andExpect(content().json("[]", true))
     }
 
     @Test
-    @Order(2)
-    fun `2 - Add first user`() {
-        val passedJsonString = """
-            {
-                "login": "xfaseex",
-                "password": "TestPassword",
-                "firstName": "Kirill",
-                "age": 19
-            }
-        """.trimIndent()
+    fun `Should return list of users`() {
 
-        val request = post(baseUrl).contentType(jsonContentType).content(passedJsonString)
+        // Given
+        val user1 = User(1, "qjnzero", "lolpassword", "Kirill", 21)
+        val user2 = User(2, "qjnzeroFAKE", "lolpasswordFAKEABLE", "Vasya", 41)
+        val users = listOf(user1, user2)
 
-        val resultJsonString = """
-            {
-                "id": 1,
-                "login": "xfaseex",
-                "password": "TestPassword",
-                "firstName": "Kirill",
-                "age": 19
-            }
-        """.trimIndent()
+        // When
+        Mockito.`when`(userService.findAll()).thenReturn(users)
 
-        mockMvc.perform(request)
-            .andExpect(status().is2xxSuccessful)
-            .andExpect(content().json(resultJsonString, true))
-    }
-
-    @Test
-    @Order(3)
-    fun `3 - Get first movie`() {
-        val request = get("$baseUrl/1").contentType(jsonContentType)
-
-        val resultJsonString = """
-            {
-                "id": 1,
-                "login": "xfaseex",
-                "password": "TestPassword",
-                "firstName": "Kirill",
-                "age": 19
-            }
-        """.trimIndent()
-
-        mockMvc.perform(request)
-            .andExpect(status().is2xxSuccessful)
-            .andExpect(content().json(resultJsonString, true))
-    }
-
-    @Test
-    @Order(4)
-    fun `4 - Get list of movies, with one movie`() {
-        val request = get(baseUrl).contentType(jsonContentType)
-
-        val resultJsonString = """
-            [
-                {
-                    "id": 1,
-                    "login": "xfaseex",
-                    "password": "TestPassword",
-                    "firstName": "Kirill",
-                    "age": 19
-                }
-            ]
-        """.trimIndent()
-
-        mockMvc.perform(request)
+        // Verify
+        mockMvc.perform(get("/api/v1/users"))
             .andExpect(status().isOk)
-            .andExpect(content().json(resultJsonString, true))
+            .andExpect(content().contentType("application/json"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].login").value("qjnzero"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].password").value("lolpassword"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName").value("Kirill"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(21))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].login").value("qjnzeroFAKE"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].password").value("lolpasswordFAKEABLE"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].firstName").value("Vasya"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].age").value(41))
     }
 
     @Test
-    @Order(5)
-    fun `5 - Delete first movie`() {
-        val request = delete("$baseUrl/1").contentType(jsonContentType)
+    fun `Should delete user`() {
 
-        mockMvc.perform(request).andExpect(status().isOk)
+        // Verify
+        mockMvc.perform(delete("/api/v1/users/1"))
+            .andExpect(status().isOk)
     }
-
 }

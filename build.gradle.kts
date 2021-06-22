d8e948c76cfcb2bd52df9e1a0cfbc7d8c3e0f939
Kirill Ostapchuk <k.ostapchuk@itransition.com>
2021-06-22 18:46:06 +0300

    refactor: upd dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,13 +23,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("com.h2database:h2")
+    runtimeOnly("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.rest-assured:kotlin-extensions:4.4.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")

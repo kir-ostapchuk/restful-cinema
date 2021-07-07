06e6e2f8a5429192957a4da1b804d27dd924caf3
Kirill Ostapchuk <k.ostapchuk@itransition.com>
2021-07-07 17:32:37 +0300

    feat: add swagger2

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation ("io.springfox:springfox-swagger2:2.9.2")
+    implementation ("io.springfox:springfox-swagger-ui:2.9.2")
     runtimeOnly("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.rest-assured:kotlin-extensions:4.4.0")

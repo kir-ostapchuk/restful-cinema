f6efe276289c150f4ab85dbbe4d35f1bd626393d
Kirill Ostapchuk <k.ostapchuk@itransition.com>
2021-07-07 17:48:00 +0300

    refactor: comment the previous code and insert new one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,68 @@
+// import React from 'react';
+// import RetrieveAllUsersComponent from "./user/RetrieveAllUsersComponent";
+// import UserDeleteComponent from "./user/UserDeleteComponent";
+// import UserPostComponent from "./user/UserPostComponent";
+// import RetrieveUserComponent from "./user/RetrieveUserComponent";
+//
+// class App extends React.Component {
+//
+//     render() {
+//         return (
+//             <>
+//                 {/*<MovieDeleteComponent/>*/}
+//                 {/*<RetrieveMovieComponent/>*/}
+//                 {/*<RetrieveAllMoviesComponent/>*/}
+//                 {/*<MoviePostComponent/>*/}
+//
+//                 <UserDeleteComponent/>
+//                 <RetrieveUserComponent/>
+//                 <RetrieveAllUsersComponent/>
+//                 <UserPostComponent/>
+//             </>
+//         );
+//     }
+// }
+//
+// export default App;
+
 import React from 'react';
-import RetrieveAllUsersComponent from "./user/RetrieveAllUsersComponent";
-import UserDeleteComponent from "./user/UserDeleteComponent";
-import UserPostComponent from "./user/UserPostComponent";
-import RetrieveUserComponent from "./user/RetrieveUserComponent";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-class App extends React.Component {
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
 
-    render() {
-        return (
-            <>
-                {/*<MovieDeleteComponent/>*/}
-                {/*<RetrieveMovieComponent/>*/}
-                {/*<RetrieveAllMoviesComponent/>*/}
-                {/*<MoviePostComponent/>*/}
+function App() {
+    return (<Router>
+            <div className="App">
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                    <div className="container">
+                        <Link className="navbar-brand" to={"/sign-in"}>positronX.io</Link>
+                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to={"/sign-in"}>Login</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
 
-                <UserDeleteComponent/>
-                <RetrieveUserComponent/>
-                <RetrieveAllUsersComponent/>
-                <UserPostComponent/>
-            </>
-        );
-    }
+                <div className="auth-wrapper">
+                    <div className="auth-inner">
+                        <Switch>
+                            <Route exact path='/' component={Login} />
+                            <Route path="/sign-in" component={Login} />
+                            <Route path="/sign-up" component={SignUp} />
+                        </Switch>
+                    </div>
+                </div>
+            </div></Router>
+    );
 }
 
 export default App;

434070e670dc85ed66e15db279615b5744f88568
Kirill Ostapchuk <k.ostapchuk@itransition.com>
2021-07-13 13:42:58 +0300

    feat: add general api docs

--- src/main/kotlin/com/itransition/web/cinema/CinemaApplication.kt
+++ src/main/kotlin/com/itransition/web/cinema/CinemaApplication.kt
@@ -1,11 +1,19 @@
 package com.itransition.web.cinema
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
-@EnableSwagger2
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = Info(
+        title = "Restful cinema API", version = "1.0.0", description = "Restful cinema API",
+        contact = Contact(email = "xfaseex1@gmail.com")
+    ),
+//    servers = [Server(url = "http://localhost:8080/api/v1", description = "Dev server")]
+)
 class CinemaApplication
 
 fun main(args: Array<String>) {
